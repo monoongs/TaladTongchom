53a5c690cf38680b5e89ad8aa3e6485c7e9b47ba
monoongs <monoongs@gmai.com>
2018-07-13 02:42:34 +0700

    First Commit

---
+++ js/boy.js
@@ -0,0 +1,14 @@
+<script language="JavaScript">
+function chkConfirm()
+{
+if(confirm('ต้องการไป Facebook Aong ?')==true)
+{
+window.location = 'https://www.facebook.com/oongnaja.ja/';
+}
+else
+{
+alert('แย่ !');
+}
+}
+</script>
+
---
+++ js/command.js
@@ -0,0 +1,15 @@
+var myImge=document.getElementById("myPhoto");
+var imageArray=["./img/a1.png","./img/a2.png"];
+var imageIndex=0;
+function changeImage(){
+	myPhoto.setAttribute("src", imageArray[imageIndex]);
+	imageIndex++;
+	if (imageIndex >= imageArray.length){
+		imageIndex=0;
+	}
+}
+
+var intervalHandle = setInterval(changeImage,1500);
+myPhoto.onclick=function(){
+	clearInterval(intervalHandle);
+}
